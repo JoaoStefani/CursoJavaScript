a2b2276347163ff1bf23977754a194eb2fa34dae
LENON\Lenon <lenonbordini@live.com>
2016-10-20 22:42:17 -0200

    Demonstração - String

---
+++ 1. Variáveis/string.js
@@ -0,0 +1,32 @@
+var string = "Hello Wor...É HORA DO SHOW P*&$%@";
+console.log(string);
+
+string = string + ' Aspas "Simples"';
+console.log(string);
+
+console.log("\nComprimento da string:", string.length);
+
+//toUpperCase|toLowerCase
+// console.log("String em Uppercase".toUpperCase());
+// console.log("String em Lowercase".toLowerCase());
+
+//replace
+// console.log("O Pinho gosta de mulheres".replace("mulheres", "homens"));
+// console.log("REPLACE".replace(/[E]+/g, "")); //Regex
+
+//trim
+// console.log("     t r i m     ".trim());
+
+//split
+// console.log("Java Script".split(" "));
+
+//startsWith|endsWith
+// console.log("Batata".startsWith("Ba"));
+// console.log("Batata".endsWith("Ba"));
+
+//repeat
+// console.log("X".repeat(3));
+
+//substring|substring
+// console.log("0123456".substring(1, 4));
+// console.log("0123456".substr(1, 4));
