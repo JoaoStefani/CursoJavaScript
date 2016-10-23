6a5a4f9ce6df6e606fdd2c8a5910033f21d44347
LENON\Lenon <lenonbordini@live.com>
2016-10-23 13:22:10 -0200

    Demonstração - Escopo

--- 2. Functions/function_escope.js
+++ 2. Functions/function_escope.js
@@ -14,29 +14,3 @@ function respostaDoAgenteK(textbox1, textbox2) {
 function respostaDoPresidenteDosEUA(textbox1, textbox2) {
     textbox1.value = buscarNomeDosAgentes();
 }
-
-
-
-
-
-
-
-
-
-var variavelGlobal = 'eu sou vista em todas as funções'
-
-function escopoFuncao1(){
-    var variavel1 = 'eu sou vista somente dentro desse escopo';
-
-    function escopoFuncao2(){
-        //EU CONSIGO VER A variavel1 por que estou dentro do escopo da função 1
-        var variavel2 = 'eu sou vista somente dentro do escopo da função 2';
-    }
-
-    //EU NÃO CONSIGO VER A variavel2
-}
-
-function escopoFuncao3(){
-    //EU NÃO VEJO NEM A variavel1 NEM A variavel2
-}
-
